6942b63372e2c778be48d6913eab80cc56617704
IDIC <alumnos@alumnosuio.ute.edu.ec>
2024-12-03 20:13:29 -0500

    ValorDescuento

--- EJERCICIO 23/testValorDescuento.py
+++ EJERCICIO 23/testValorDescuento.py
@@ -2,12 +2,12 @@ import utilitarios
 
 ##Calcular descuento 
 descuento1=utilitarios.calcularValorDescuento(120.50,20)
-print(f"Test 1:{descuento1}")
+print(f"Test 1: {descuento1}")
 
 descuento2=utilitarios.calcularValorDescuento(80,30)
-print(f"Test 2:{descuento2}")
+print(f"Test 2: {descuento2}")
 
 descuento3=utilitarios.calcularValorDescuento(500,15)
-print(f"Test 2:{descuento3}")
+print(f"Test 2: {descuento3}")
 
 
--- EJERCICIO 23/utilitarios.py
+++ EJERCICIO 23/utilitarios.py
@@ -2,3 +2,14 @@ def calcularPromedio(val1,val2,val3):
     calculo=val1+val2+val3
     promedio1=calculo/3
     return promedio1
+
+
+
+def calcularValorDescuento(precio,porcentajeDescuento):
+    calcularDescuento= precio * porcentajeDescuento
+    descuentos = calcularDescuento/100
+    return descuentos 
+
+
+
+    
